50962e47bb00ccd8f4d0bbbca27f46fd3516d1ce
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-04 15:15:53 +0500

    guest and wedding model added

--- wedding/guest/models.py
+++ wedding/guest/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
+from wedding_api.models import Wedding
+from users.utils import check_phone_number
 
-# Create your models here.
+
+class Guest(models.Model):
+    wedding = models.ForeignKey(Wedding, on_delete=models.CASCADE, related_name="guests")
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    qr_code = models.CharField(max_length=255)
+    is_attending = models.BooleanField(default=False)
+    total_donations = models.DecimalField(max_digits=10, decimal_places=2, default=0.0, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if check_phone_number(self.phone_number):
+            super().save(*args, **kwargs)
+        else:
+            raise ValidationError("Invalid phone number format.")
--- wedding/wedding_api/models.py
+++ wedding/wedding_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Wedding(models.Model):
+    groom_name = models.CharField(max_length=100)
+    bride_name = models.CharField(max_length=100)
+    groom_info = models.TextField(null=True, blank=True)  # Info like age, origin, etc.
+    bride_info = models.TextField(null=True, blank=True)  # Info like age, origin, etc.
+    wedding_date = models.DateTimeField()
+    vanue_name = models.CharField(max_length=255) #The specific name of the venue where the wedding is held.
+    location = models.CharField(max_length=255)  # Integrated with Yandex Maps
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    invitation_style = models.CharField(max_length=255, null=True, blank=True)
+    qr_code_style = models.CharField(max_length=255, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # One user, one wedding
+    donation_card_number = models.CharField(max_length=20, blank=True, null=True)
