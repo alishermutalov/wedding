4c0f7854c4d92d79031a870f2af921c9b4d8b8b1
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-18 23:50:27 +0500

    edited something

--- wedding/payment/models.py
+++ wedding/payment/models.py
@@ -5,7 +5,7 @@ from subscription.models import TariffPlan, Subscription
 
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payments")
-    tariff_plan = models.ForeignKey(TariffPlan, on_delete=models.CASCADE)
+    tariff_plan = models.ForeignKey(TariffPlan, on_delete=models.CASCADE, verbose_name="Tariff Plan ")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed'), ('failed', 'Failed')])
