4f827ab2621b5af8fb3a57d2cc66a68183ce4d4d
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-27 23:20:35 +0500

    added something in wedding serializer

--- wedding/apps/wedding_api/serializers.py
+++ wedding/apps/wedding_api/serializers.py
@@ -5,3 +5,20 @@ from rest_framework import serializers
 class WeddingSerializer(serializers.ModelSerializer):
     user = serializers.CharField(read_only=True, required=False)
     
+    class Meta:
+        model = Wedding
+        fields = [
+            'user',
+            'groom_name',
+            'bride_name',
+            'groom_info',
+            'bride_info',
+            'wedding_date',
+            'vanue_name',
+            'location',
+            'latitude',
+            'longitude',
+            'invitation_style',
+            'qr_code_style',
+            'donation_card_number',
+        ]
