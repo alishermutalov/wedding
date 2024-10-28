7a0dbd32e2fdf621ccd5f394e2f7b7c0b20866db
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-28 21:30:59 +0500

    List Subscription added

--- wedding/apps/subscription/views.py
+++ wedding/apps/subscription/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .serializers import SubscriptionSerializer
+from .models import Subscription
+
+class SubscriptionListAPIView(ListAPIView):
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated,]
+    
+    def get_queryset(self):
+        return Subscription.objects.filter(user=self.request.user)
