3066f7d4212267829d9a8e408c571173a80824ed
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-10-28 21:34:44 +0500

    edited subs. urls

--- wedding/apps/subscription/urls.py
+++ wedding/apps/subscription/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import SubscriptionListAPIView
 
 urlpatterns = [
-    path('subscriptions/', SubscriptionListAPIView.as_view() )
+    path('', SubscriptionListAPIView.as_view() )
 ]
